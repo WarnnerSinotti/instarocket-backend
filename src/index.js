d0b9030e0da3813c4adc98810aed1d7b7791c79d
Warnner Sinotti <warnner13@gmail.com>
2021-05-09 17:46:06 -0300

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
 const app = express();
 
+const server = require('http').server(app);
+const io = require('socket.io')(server);
+
+
 mongoose.connect('mongodb+srv://semana:semana@cluster0.gnhoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{useNewUrlParser: true,});
+{useUnifiedTopology: true, useNewUrlParser: true,});
+
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    next();
+})
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
+
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/uploud');
+
+
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+
 const routes = new express.Router();
+const uploud = multer(uploadConfig);
 
+routes.get('/posts', PostController.index);
+routes.post('/posts', uploud.single('image'), PostController.store);
 
-routes.post('/posts', PostController.store);
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
